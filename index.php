6bbba1eb99adc6d193556c044a517ad14604a36f
DavideTumolo11 <davidetumolo11@gmail.com>
2024-04-04 12:34:07 +0200

    courses

--- index.php
+++ index.php
@@ -9,10 +9,98 @@ define('DB_NAME', '01_university');
 $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 
-var_dump($connection);
+//var_dump($connection);
 
 
 if ($connection && $connection->connect_error) {
     echo 'database connection failed';
     die;
 }
+
+$sqlYear = "SELECT * FROM `students` WHERE YEAR (`date_of_birth`) = 1990";
+$resultYear = $connection->query($sqlYear);
+
+$sqlDegrees = "SELECT `name`, `period`,`year` FROM `courses` WHERE `period` = 'I semestre' and `year` = 1";
+$resultDegrees = $connection->query($sqlDegrees);
+
+$sqlAllDegrees = "SELECT `name` FROM `degrees`";
+$resultAllDegrees = $connection->query($sqlAllDegrees);
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table</title>
+</head>
+
+<body>
+
+
+    <?php while ($row = $resultYear->fetch_assoc()) :
+
+        ['date_of_birth' => $date_of_birth] = $row;
+
+    ?>
+
+
+        <div>
+            <p>Students born in 1990 <strong><?= $date_of_birth ?></strong></p>
+        </div>
+
+
+
+
+    <?php endwhile; ?>
+
+
+
+    <?php while ($row = $resultDegrees->fetch_assoc()) :
+
+        ['name' => $name] = $row;
+        ['period' => $period] = $row;
+
+    ?>
+
+
+        <div>
+            <p>Name of Courses <strong><?= $name ?></strong></p>
+            <p>period <strong><?= $period ?></strong></p>
+        </div>
+
+
+
+    <?php endwhile; ?>
+
+
+
+    <?php while ($row = $resultAllDegrees->fetch_assoc()) :
+
+        ['name' => $name] = $row;
+
+    ?>
+
+
+        <div>
+            <p>All degrees <strong><?= $name ?></strong></p>
+        </div>
+
+
+
+    <?php endwhile; ?>
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+Esercizio di oggi: PHP university
+nome repo: php-mysqli-university
+Eccoci all'ultimo esercizio prima della prossima milestone che assegneremo Giovedi 4 come scritto sopra da Filippo!
+Oggi chiudiamo un primo cerchio unendo un pó di puntini visti.
+
+In questa esercitazione, applicheremo le nostre conoscenze di PHP e quelle che abbiamo maturato sui database e ci connetteremo per la prima volta al nostro database dell'universita
+(quello che avete usato fino ad oggi per fare le query) e grazie ai tools di PHP ne stamperemo i dati in pagina:parrotjedi:
+
+Descrizione:
+
+Ripercorrendo i passaggi visti in classe statesa realizzate una o piú pagine in cui stampare i dati ottenuti con le query viste nei giorni scorsi.
+Ad esempio connettersi al database tramite PHP:
+
+Selezionare e stampare tutti gli studenti nati nel 1990 (160)
+
+Selezionare e stampare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
+Selezionare e stampare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+BONUS:
+Far funzionare i filtri delle query dinamicamente come visto in classe. Ad esempio, se invece di quelli nati nel 1990 volessi tramite un apposito input filtrare solo quelli nati nel 2000?
+Puoi potenzialmente stampare ogni query in pagina di quelle fatte nei giorni scorsi. Fallo e DIVERTITI! :parrotjedi:
+Magari.. puoi fare bene il CSS ora.
